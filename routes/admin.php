31d54037c4c460084eb5ce0f2eab2739a7033864
mohamedmagdy233 <amomatter48@gmail.com>
2025-03-08 17:28:12 +0200

    matter 2025-03-08 17:28:12

--- routes/admin.php
+++ routes/admin.php
@@ -102,3 +102,9 @@ Route::group(
 );
 
 
+
+Route::resourceWithDeleteSelected('plan_subscriptions', \App\Http\Controllers\Admin\PlanSubscriptionController::class);
+
+Route::resourceWithDeleteSelected('investors', \App\Http\Controllers\Admin\InvestorController::class);
+
+Route::resourceWithDeleteSelected('clients', \App\Http\Controllers\Admin\ClientController::class);
