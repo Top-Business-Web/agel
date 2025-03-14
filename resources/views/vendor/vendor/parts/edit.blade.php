dbda7cae79b59b7503d2426c5fdf53454a8efc19
mohamedmagdy233 <amomatter48@gmail.com>
2025-03-14 09:55:38 +0200

    matter 2025-03-14 09:55:38

--- resources/views/vendor/vendor/parts/edit.blade.php
+++ resources/views/vendor/vendor/parts/edit.blade.php
@@ -9,18 +9,20 @@
             <div class="col-12">
                 <div class="form-group">
                     <label for="image" class="form-control-label">الصورة
-</label>
+                    </label>
                     <input type="file" class="dropify" name="image" id="image"
-                        data-default-file="{{ $obj->image }}">
+                           data-default-file="{{ getFile($obj->image)}}">
                 </div>
             </div>
 
-            <div class="col-6">
+            <div class="col-12">
                 <div class="form-group">
-                    <label for="city_id" class="form-control-label">المدينه</label>
-                    <select placeholder="{{ trns('-- select_city --') }}" class="form-control" name="city_id" id="city_id">
-                        @foreach ($cities as $city)
-                            <option value="{{$city->id }}">{{$city->name}}</option>
+                    <label for="region_id" class="form-control-label">اسم الحي</label>
+                    <select class="form-control" name="region_id" id="region_id">
+                        <option value="" selected disabled>اختر الحي</option>
+                        @foreach ($regions as $region)
+                            <option
+                                value="{{$region->id }}" {{ $obj->region_id == $region->id ? 'selected' : '' }}>{{$region->name}}</option>
                         @endforeach
                     </select>
                 </div>
@@ -29,57 +31,54 @@
             <div class="col-6">
                 <div class="form-group">
                     <label for="name" class="form-control-label">الاسم
-</label>
+                    </label>
                     <input type="text" class="form-control" name="name" id="name"
-                        value="{{ $obj->name }}">
+                           value="{{ $obj->name }}">
                 </div>
             </div>
-
 
 
             <div class="col-6">
                 <div class="form-group">
                     <label for="email" class="form-control-label">البريد الإلكتروني
-</label>
+                    </label>
                     <input type="email" class="form-control" name="email" id="email"
-                        value="{{ $obj->email }}">
+                           value="{{ $obj->email }}">
                 </div>
             </div>
 
             <div class="col-6">
                 <div class="form-group">
-                    <label for="name" class="form-control-label">{{ trns('phone') }}</label>
+                    <label for="name" class="form-control-label">رقم الجوال</label>
                     <input type="number" class="form-control" name="phone" maxlength="11" id="name"
-                        value="{{ $obj->phone }}">
+                           value="{{ $obj->phone }}">
                 </div>
             </div>
 
             <div class="col-6">
                 <div class="form-group">
-                    <label for="name" class="form-control-label">{{ trns('national_id') }}</label>
+                    <label for="name" class="form-control-label">رقم الهوية</label>
                     <input type="number" class="form-control" name="national_id" minlength="14" id="name"
-                        value="{{ $obj->national_id }}">
+                           value="{{ $obj->national_id }}">
                 </div>
             </div>
 
             <div class="col-6">
                 <div class="form-group">
-                    <label for="password" class="form-control-label">{{ trns('password') }}</label>
+                    <label for="password" class="form-control-label">كلمة المرور</label>
                     <input type="password" class="form-control" name="password" id="password">
                 </div>
             </div>
 
             <div class="col-6">
                 <div class="form-group">
-                    <label for="password" class="form-control-label">{{ trns('password_confirmation') }}</label>
+                    <label for="password" class="form-control-label">تأكيد كلمة المرور</label>
                     <input type="password" class="form-control" name="password_confirmation" id="password">
                 </div>
             </div>
 
 
-
         </div>
-
 
 
         <div class="modal-footer">
