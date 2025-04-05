884aa0876c420a1122b9673fd4a97481e3faed8f
mohamedmagdy233 <amomatter48@gmail.com>
2025-04-05 10:30:02 +0200

    matter 2025-04-05 10:30:02

--- app/Services/Admin/SettingService.php
+++ app/Services/Admin/SettingService.php
@@ -4,8 +4,6 @@ namespace App\Services\Admin;
 
 use App\Models\Setting as ObjModel;
 use App\Services\BaseService;
-use Illuminate\Support\Facades\Auth;
-use Yajra\DataTables\DataTables;
 
 class SettingService extends BaseService
 {
@@ -21,79 +19,19 @@ class SettingService extends BaseService
     {
 
         $settings=$this->model->all();
-        if ($request->ajax()) {
-            $obj = $this->getDataTable();
-            return DataTables::of($obj)
-            ->editColumn('key', function ($obj) {
-                return $obj->key;
-            })
-            ->editColumn('value', function ($obj) {
-                return $obj->value;
-            })
-                ->addColumn('action', function ($obj) {
-                        $buttons = ' ';
 
-                        $buttons .= '
-                        <button type="button" data-id="' . $obj->id . '" class="btn btn-pill btn-info-light editBtn">
-                            <i class="fa fa-edit"></i>
-                        </button>
-                    ';
-
-                        $buttons .= '<button class="btn btn-pill btn-danger-light" data-bs-toggle="modal"
-                            data-bs-target="#delete_modal" data-id="' . $obj->id . '" data-title="' . $obj->name . '">
-                            <i class="fas fa-trash"></i>
-                        </button>';
-                    return $buttons;
-                })
-                ->addIndexColumn()
-                ->escapeColumns([])
-                ->make(true);
-        } else {
             return view($this->folder . '/index', [
                 'updateRoute' => route("{$this->route}.update", $request),
                 'bladeName' => ($this->route),
                 'route' => $this->route,
                 'settings'=>$settings
             ]);
-        }
+
     }
 
-    public function create()
-    {
-
-        return view("{$this->folder}/parts/create", [
-            'storeRoute' => route("{$this->route}.store"),
-
-        ]);
-    }
-
-    public function store($data): \Illuminate\Http\JsonResponse
-    {
-        if (isset($data['image'])) {
-            $data['image'] = $this->handleFile($data['image'], 'Setting');
-        }
-
-        try {
-            $this->createData($data);
-            return response()->json(['status' => 200, 'message' => ('Data created successfully.')]);
-        } catch (\Exception $e) {
-            return response()->json(['status' => 500, 'message' => ('Something went wrong.'), ('error') => $e->getMessage()]);
-        }
-    }
-
-    public function edit($obj)
-    {
-        $settings=$this->model->all();
-        return view("{$this->folder}/parts/edit", [
-            'obj' => $obj,
-            'updateRoute' => route("{$this->route}.update"),
-            'settings'=>$settings
-        ]);
-    }
 
     public function update(array $data)
     {
-//        dd($data);
         try {
             // Handle file uploads
             $files = ['logo', 'fav_icon', 'loader'];
@@ -127,11 +65,11 @@ class SettingService extends BaseService
 
 
 
-            return response()->json(['status' => 200, 'message' => 'Settings stored or updated successfully.']);
+            return response()->json(['status' => 200, 'message' => 'تمت العملية بنجاح']);
         } catch (\Exception $e) {
             return response()->json([
                 'status' => 500,
-                'message' => 'Something went wrong.',
+                'message' => 'حدث خطأ أثناء العملية',
                 'error' => $e->getMessage()
             ]);
         }
