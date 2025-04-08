e5ab8384a54572194944d31e7fc431cf6b5a5e46
mohamed <amomatter48@gmail.com>
2025-04-08 16:36:23 +0200

    matter 2025-04-08 16:36:23

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             AreaSeeder::class,
             RegionSeeder::class
         ]);
+        // $this->call(PlanSeeder::class);
         $this->call(FreePlanSeeder::class);
 
         $this->call(BranchSeeder::class);
