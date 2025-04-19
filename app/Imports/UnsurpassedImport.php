94c7c5044fb38c3c5504527ea59e44769d102bed
ahmedesmaelgamal <ahmedesmaelgamal@gmail.com>
2025-04-19 19:26:30 +0200

    edit

--- app/Imports/UnsurpassedImport.php
+++ app/Imports/UnsurpassedImport.php
@@ -27,7 +27,7 @@ class UnsurpassedImport implements ToCollection, WithHeadingRow, WithValidation
     public function collection(Collection $collection)
     {
         foreach ($collection as $row) {
-            Unsurpassed::create([
+            Unsurpassed::updateOrCreate([
                 'name' => $row['name'] ?? null, // Handle different column names
                 'phone' => '+966'.$row['phone']  ?? null,
                 'national_id' => $row['national_id']  ?? null,
@@ -50,6 +50,21 @@ class UnsurpassedImport implements ToCollection, WithHeadingRow, WithValidation
             'national_id' => 'required|numeric|digits:10|unique:unsurpasseds,national_id',
             'office_name' => 'nullable|max:255',
             'office_phone' => 'nullable|regex:/^[0-9]{9}$/|unique:unsurpasseds,office_phone|unique:unsurpasseds,phone',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'phone.required' => 'The phone field is required.',
+            'phone.regex' => 'The phone number must contain exactly 9 digits.',
+            'phone.unique' => 'The phone number must be unique and not match any existing phone or office phone.',
+            'national_id.required' => 'The national ID field is required.',
+            'national_id.numeric' => 'The national ID must be a numeric value.',
+            'national_id.digits' => 'The national ID must be exactly 10 digits.',
+            'national_id.unique' => 'The national ID already exists in the database.',
+            'office_name.max' => 'The office name must not exceed 255 characters.',
+            'office_phone.regex' => 'The office phone number must contain exactly 9 digits.',
+            'office_phone.unique' => 'The office phone number must be unique and not match any existing office phone or phone.',
         ];
     }
 
