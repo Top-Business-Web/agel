da31f3b530c1cabbee0fa2f492a6f3537f340579
mohamedmagdy233 <amomatter48@gmail.com>
2025-05-19 10:46:47 +0300

    matter 2025-05-19 10:46:47

--- resources/views/vendor/investor/parts/investor_stocks_summary.blade.php
+++ resources/views/vendor/investor/parts/investor_stocks_summary.blade.php
@@ -1,12 +1,38 @@
-<h3>المخزون الخاص {{ $investor->name }}</h3>
-<label>الكمية المضافه</label>
-<h3>{{$stocksWithTheSameCategoryInAddOperation->sum('quantity')}}</h3>
-<label>السعر الاحمالي للكميه المضافه</label>
-<h3>{{$stocksWithTheSameCategoryInAddOperation->sum('total_price_add')}}</h3>
-<label>الكمية المنقصه</label>
-<h3>{{$stocksWithTheSameCategoryInSellOperation->sum('quantity')}}</h3>
-<label>السعر الاحمالي للكميه المنقصه</label>
-<h3>{{$stocksWithTheSameCategoryInSellOperation->sum('total_price_sub')}}</h3>
-<label>الكميه المتبقيه</label>
-<h3>{{$stocksWithTheSameCategoryInAddOperation->sum('quantity') - $stocksWithTheSameCategoryInSellOperation->sum('quantity')}}</h3>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap');
+
+        p,h3{
+            font-family: "Cairo", sans-serif;
+
+        }
+    </style>
+
+<div style="height: 100vh; display: flex; justify-content: center; align-items: center; flex-direction: column; direction: rtl;">
+    <div class="container">
+        <div style="box-shadow: 0 5px 83px 0 rgba(40, 40, 40, 0.11); padding: 20px; border-radius: 10px; width: 400px;">
+        <h3 style="text-align: center;">المخزون الخاص {{ $investor->name }}</h3>
+        <div style="display: flex">
+            <p>الكمية المضافه</p>
+            <p style="font-weight: bold; margin-right: 10px;">{{$stocksWithTheSameCategoryInAddOperation->sum('quantity')}}</p>
+        </div>
+        <div style="display: flex">
+            <p>السعر الاحمالي للكميه المضافه</p>
+            <p style="font-weight: bold; margin-right: 10px;">{{$stocksWithTheSameCategoryInAddOperation->sum('total_price_add')}}</p>
+        </div>
+        <div style="display: flex">
+            <p>الكمية المنقصه</p>
+            <p style="font-weight: bold; margin-right: 10px;">{{$stocksWithTheSameCategoryInSellOperation->sum('quantity')}}</p>
+        </div>
+       <div style="display: flex">
+        <p>السعر الاحمالي للكميه المنقصه</p>
+        <p style="font-weight: bold; margin-right: 10px;">{{$stocksWithTheSameCategoryInSellOperation->sum('total_price_sub')}}</p>
+       </div>
+       <hr>
+        <div style="display: flex">
+            <p>الكميه المتبقيه</p>
+            <p style="font-weight: bold; margin-right: 10px;">{{$stocksWithTheSameCategoryInAddOperation->sum('quantity') - $stocksWithTheSameCategoryInSellOperation->sum('quantity')}}</p>
+        </div>
+    </div>
+    </div>
+</div>
 
